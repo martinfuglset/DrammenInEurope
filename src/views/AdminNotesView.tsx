8a00eaccab99c6defbe39a1f7074e7fca0682b7c
Elena Othilie Solberg <elena@solberg.no>
2026-02-16 18:27:47 +0100

    trying again

--- src/views/AdminNotesView.tsx
+++ src/views/AdminNotesView.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useStore, selectIsAdmin } from '../store';
-import type { AdminNote, AdminList, AdminListItem } from '../types';
 import {
   ArrowLeft,
   Plus,
